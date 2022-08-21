8f503d57dd5f0577ca04c7bcd1c91c7cc6da3b12
doiko123 <ayanottyz@gmail.com>
2022-08-22 01:13:21 +0900

    リスト表示する用のデータについてMenuListItemのListとしてファイルに定義した

---
+++ task_03/lib/data/menu_list_data.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:task_03/models/types/menu_list_item.dart';
+
+List<MenuListItem> menuListData = [
+  MenuListItem(
+    textString: 'ゴリドナルド',
+    icon: const Icon(
+      Icons.fastfood,
+    ),
+  ),
+  MenuListItem(
+    textString: 'ゴリライレブン',
+    icon: const Icon(
+      Icons.local_convenience_store_rounded,
+    ),
+  ),
+  MenuListItem(
+    textString: 'ゴリゼリヤ',
+    icon: const Icon(Icons.local_dining),
+  ),
+  MenuListItem(
+    textString: 'エネゴリ',
+    icon: const Icon(
+      Icons.local_gas_station,
+    ),
+  ),
+  MenuListItem(
+    textString: 'ゴリンランドリー',
+    icon: const Icon(
+      Icons.local_laundry_service,
+    ),
+  ),
+  MenuListItem(
+    textString: 'ゴリノピザ',
+    icon: const Icon(
+      Icons.local_pizza_sharp,
+    ),
+  ),
+];
