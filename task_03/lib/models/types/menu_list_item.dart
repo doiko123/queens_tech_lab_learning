e1329a00b283bcfe91f5f1f1536f0aad7b50b684
doiko123 <ayanottyz@gmail.com>
2022-08-22 01:11:47 +0900

    メニューリストのデータ（アイコンとテキストのセット）の型を定義するためのmenu_list_item_modelを定義

---
+++ task_03/lib/models/types/menu_list_item.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MenuListItem {
+  String textString;
+  Icon icon;
+
+  MenuListItem({
+    required this.textString,
+    required this.icon,
+  });
+}
