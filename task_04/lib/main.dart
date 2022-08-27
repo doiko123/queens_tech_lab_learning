e29390476f102bca23c2e233292489f6e9e84d02
doiko123 <ayanottyz@gmail.com>
2022-08-27 15:25:41 +0900

    home_pageを別Widgetに切り出し

--- task_04/lib/main.dart
+++ task_04/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_04/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,28 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Task 04'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body:
-          Container(), // This trailing comma makes auto-formatting nicer for build methods.
+      home: const HomePage(title: 'Task 04'),
     );
   }
 }
