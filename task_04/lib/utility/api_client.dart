219724e185de0ce953cbbc6964fc8ee39c284f9c
doiko123 <ayanottyz@gmail.com>
2022-09-03 01:35:09 +0900

    APIClientについてExtensionの変更を反映した形に修正／linterのerrorにより書き方を改善

--- task_04/lib/utility/api_client.dart
+++ task_04/lib/utility/api_client.dart
@@ -1,14 +1,19 @@
+import 'package:task_04/models/event.dart';
+import 'package:task_04/models/events_data.dart';
 import 'package:task_04/utility/api_client_extension.dart';
 
 class APIClient extends APIClientExtension {
-  // イベント一覧を取得を取得
+  final apiClient = APIClient();
+  // イベント一覧を取得
   // GET /events
-  Future<List<Event>> listEvents() async {
-    final response = await dio.get(
+  Future<List<Event>> getEvents() async {
+    final response = await apiClient.get<EventsData>(
       'event',
     );
-    return (response.data['events'] as List)
-        .map((index) => Event.fromJson(index))
-        .toList();
+    final data = EventsData.fromJson(
+      response.data! as Map<String, dynamic>,
+    );
+
+    return data.events.toList();
   }
 }
