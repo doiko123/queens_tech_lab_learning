42d16dbbb4d44fa83146b34e91f09a37e9f34984
doiko123 <ayanottyz@gmail.com>
2022-09-03 00:59:24 +0900

    APIClientExtensionについてDioMixinをmixinする形に変更

--- task_04/lib/utility/api_client_extension.dart
+++ task_04/lib/utility/api_client_extension.dart
@@ -1,24 +1,23 @@
 import 'package:dio/dio.dart';
 import 'package:task_04/utility/environment_variables.dart';
 
-// TODO(doiko): Dioのclassを継承するように書き換える
-// （Dioのプロパティをオーバーロードする感じで書けるので）
-class APIClientExtension {
-  static BaseOptions options = BaseOptions(
+
+class APIClientExtension with DioMixin implements Dio {
+  APIClientExtension() {
+    options = BaseOptions(
       // TODO(doiko): header情報を受け取って与えられるようにする
       baseUrl: EnvironmentVariables.connpassBaseURL,
       validateStatus: (status) {
         return status! < 300;
-      });
-
-  Dio dio = Dio(options);
-
-  APIClientExtension() {
-    dio.interceptors.addAll([
-      LogInterceptor(
-        requestBody: true,
-        responseBody: true,
-      ),
-    ]);
+      },
+    );
+    interceptors.addAll(
+      [
+        LogInterceptor(
+          requestBody: true,
+          responseBody: true,
+        ),
+      ],
+    );
   }
 }
