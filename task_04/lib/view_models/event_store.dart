f51a058924ff7a9cfc04ff66d628d0daf098038d
doiko123 <ayanottyz@gmail.com>
2022-09-24 01:48:13 +0900

    print文をいったん削除

--- task_04/lib/view_models/event_store.dart
+++ task_04/lib/view_models/event_store.dart
@@ -13,12 +13,10 @@ class EventStore extends ChangeNotifier {
   Future<void> getEvents({
     required String keyword,
   }) async {
-    print(_events);
     final response = await _apiClient.getEvents(
       keyword: keyword,
     );
     _events = response ?? [];
-    print(_events);
     // 変更を通知する
     notifyListeners();
   }
