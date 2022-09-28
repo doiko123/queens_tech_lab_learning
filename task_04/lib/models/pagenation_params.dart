94411cf30764072a653bb141b69e29c92e2415c8
doiko123 <ayanottyz@gmail.com>
2022-09-28 21:01:08 +0900

    ページネーション用のリクエストパラメータのエンティティを用意しflutter pub run build_runner buildした

---
+++ task_04/lib/models/pagenation_params.dart
@@ -0,0 +1,45 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'pagenation_params.g.dart';
+
+@JsonSerializable()
+// イベント取得におけるページネーション用
+class PaginationParams {
+  PaginationParams({
+    this.start,
+    this.order,
+    this.count,
+  });
+
+  factory PaginationParams.fromJson(Map<String, dynamic> json) =>
+      _$PaginationParamsFromJson(json);
+
+  int? start = 1; // 検索の開始位置
+  OrderBy? order = OrderBy.updatedAt; // 検索結果の表示順
+  int? count = requestEventSize; // 取得件数
+
+  Map<String, dynamic> toJson() => _$PaginationParamsToJson(this);
+}
+
+enum OrderBy {
+  updatedAt, // 更新日時順
+  startedAt, // 開催日時順
+  createdAt, // 新着順
+}
+
+// enumに対応する値を返すための拡張
+extension OrderByExt on OrderBy {
+  int get order {
+    switch (this) {
+      case OrderBy.updatedAt:
+        return 1;
+      case OrderBy.startedAt:
+        return 2;
+      case OrderBy.createdAt:
+        return 3;
+    }
+  }
+}
+
+// リクエスト1回につき取得するイベントの件数
+const int requestEventSize = 10;
---
+++ task_04/lib/models/pagenation_params.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint, implicit_dynamic_parameter, implicit_dynamic_type, implicit_dynamic_method, strict_raw_type
+
+part of 'pagenation_params.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+PaginationParams _$PaginationParamsFromJson(Map<String, dynamic> json) =>
+    PaginationParams(
+      start: json['start'] as int?,
+      order: $enumDecodeNullable(_$OrderByEnumMap, json['order']),
+      count: json['count'] as int?,
+    );
+
+Map<String, dynamic> _$PaginationParamsToJson(PaginationParams instance) =>
+    <String, dynamic>{
+      'start': instance.start,
+      'order': _$OrderByEnumMap[instance.order],
+      'count': instance.count,
+    };
+
+const _$OrderByEnumMap = {
+  OrderBy.updatedAt: 'updatedAt',
+  OrderBy.startedAt: 'startedAt',
+  OrderBy.createdAt: 'createdAt',
+};
